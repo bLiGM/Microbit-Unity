c03e1a3aaa8216999cd9d8544c59ebbd291a9bfa
bLiGM <ucabbl2@ucl.ac.uk>
2016-08-09 14:34:56 +0100

    Initial version of my Unity scripts to allow the use of the BBC Microbit as a Unity Controller.

---
+++ Microbit Unity/Assets/Scripts/Controllers/SerialToInput.cs
@@ -0,0 +1,185 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SerialToInput : MonoBehaviour {
+	public SerialStream serialInput;
+	public static bool isSerialInit = false;
+
+	//rotation + button presses from the serial input
+	private Vector3 rotation;
+	private bool buttonAPressed;
+	private bool buttonBPressed;
+	private bool buttonAHeld;
+	private bool buttonBHeld;
+	private float buttonAStart;
+	private float buttonBStart;
+	private bool buttonAHoldCheck;
+	private bool buttonBHoldCheck;
+
+
+	private bool debug;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+		//if an instance of this object already exists, destroy this.
+		if (isSerialInit == true) {
+			printDebug ("Destorying SerialToController");
+			Destroy (this.gameObject);
+		}
+		isSerialInit = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateInputs (serialInput.getLine());
+		/*printDebug (buttonA);
+		printDebug (buttonB);
+		printDebug (rotation.ToString());*/
+	}
+
+	/// <summary>
+	/// Updates the inputs.
+	/// </summary>
+	/// <param name="s">S.</param>
+	private void updateInputs(string s){
+		//updates the logic depending on the input of the form
+		//button,x,y,z
+		try{
+			char[] seperator = {','};
+			string[] splitString = s.Split(seperator,4);
+		// if any of them are null, exception will be thrown and the line will be ignored.
+			if(splitString[0].Equals("A")){
+				buttonAPressed = true;
+				buttonBPressed = false;
+				buttonBHeld = false;
+				buttonBHoldCheck = false;
+				checkButtonA();
+			}
+			else if(splitString[0].Equals("B")){
+				buttonBPressed = true;
+				buttonAPressed = false;
+				buttonAHeld = false;
+				buttonAHoldCheck = false;
+				checkButtonB();
+			}
+			else if(splitString[0].Equals("C")){
+				buttonAPressed = true;
+				buttonBPressed = true;
+
+				checkButtonA();
+				checkButtonB();
+
+			}
+			else{
+				buttonAPressed = false;
+				buttonBPressed = false;
+				buttonAHoldCheck = false;
+				buttonBHoldCheck = false;
+				buttonAHeld = false;
+				buttonBHeld = false;
+				
+				
+			}
+			string X = splitString[1];
+			string Y = splitString[2];
+			string Z = splitString[3];
+
+			rotation.Set((float) int.Parse(X),(float) int.Parse(Y), (float) int.Parse(Z));
+
+
+		}catch(System.Exception){
+			return;
+		}
+	}
+
+	/// <summary>
+	/// Checks the button a.
+	/// </summary>
+	private void checkButtonA(){
+		if(buttonAHoldCheck == false){
+			buttonAHoldCheck = true;
+			buttonAStart = Time.time;
+		}
+		else if(buttonAHoldCheck == true){
+			if((Time.time - buttonAStart)<0.25){
+				//Do nothing
+				buttonAHeld = false;
+			}
+			else{
+				buttonAHeld = true;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Checks the button b.
+	/// </summary>
+	private void checkButtonB(){
+		if(buttonBHoldCheck == false){
+			buttonBHoldCheck = true;
+			buttonBStart = Time.time;
+		}
+		else if(buttonBHoldCheck == true){
+			if((Time.time - buttonBStart)<0.25){
+				//Do nothing
+				buttonBHeld = false;
+			}
+			else{
+				buttonBHeld = true;
+			}
+		}
+	}
+
+
+	/// <summary>
+	/// Gets A pressed.
+	/// </summary>
+	/// <returns><c>true</c>, if A pressed was gotten, <c>false</c> otherwise.</returns>
+	public bool getAPressed(){
+		return this.buttonAPressed;
+	}
+
+
+	/// <summary>
+	/// Gets the B pressed.
+	/// </summary>
+	/// <returns><c>true</c>, if B pressed was gotten, <c>false</c> otherwise.</returns>
+	public bool getBPressed(){
+		return this.buttonBPressed;
+	}
+
+	/// <summary>
+	/// Gets A held.
+	/// </summary>
+	/// <returns><c>true</c>, if A held was gotten, <c>false</c> otherwise.</returns>
+	public bool getAHeld(){
+		return this.buttonAHeld;
+	}
+		
+	/// <summary>
+	/// Gets the B held.
+	/// </summary>
+	/// <returns><c>true</c>, if B held was gotten, <c>false</c> otherwise.</returns>
+	public bool getBHeld(){
+		return this.buttonBHeld;
+	}
+
+	/// <summary>
+	/// Gets the rotation.
+	/// </summary>
+	/// <returns>The rotation.</returns>
+	public Vector3 getRotation(){
+		return this.rotation;
+	}
+
+	private void printDebug(string s){
+		if (debug) {
+			Debug.Log (s);
+		}
+	}
+
+}
---
+++ Microbit Unity/Assets/Scripts/Serial/SerialStream.cs
@@ -0,0 +1,178 @@
+using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System;
+
+
+public class SerialStream : MonoBehaviour {
+	private SerialPort stream;
+	private string inline;
+	public string COM;
+	public int BAUD;
+	public int timeout = 0;
+	public static bool isSerialStreamInit = false;
+
+	private bool debug = false;
+
+	
+
+	// Use this for initialization
+	// Unity does not use constructors, but rather Start() and Awake()
+	void Start () {
+
+
+		//if an instance of this object already exists, destroy this.
+		if (isSerialStreamInit == true) {
+			printDebug ("Destorying duplicate SerialStream");
+			Destroy (this.gameObject);
+		}
+		isSerialStreamInit = true;
+
+		this.stream = new SerialPort (COM, BAUD);
+		if (this.timeout == 0) {
+			this.stream.ReadTimeout = 5;
+		} else {
+			this.stream.ReadTimeout = this.timeout;
+		}
+		openStream ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if ((inline = this.readStream ()) != null) {
+			//printDebug (inline);
+		}*/
+
+		StartCoroutine(AsyncReaduBit(10f));
+	}
+
+	//Send and Read CoRoutines
+	public IEnumerator AsyncReaduBit(float timeout, System.Action fail = null){
+		//timeout should be very small (microseconds);
+		float startTime = Time.time;
+		float currentTime = 0;
+		float diff = 0;
+
+		pinguBit ();
+
+		string readLine = null;
+		do {
+			try {
+				readLine = readStream ();
+			} catch (System.Exception) {
+				//printDebug(e);
+				readLine = null;
+			}
+			if(readLine != null){
+				this.inline = readLine;
+				this.stream.BaseStream.Flush();
+			}
+			else{
+				yield return new WaitForSeconds(0.016f);
+			}
+
+			currentTime = Time.time;
+			//difference in milliseconds
+			diff = (currentTime - startTime)*1000;
+		} while(diff < timeout);
+
+		if(fail !=null){
+			fail();
+		}
+		else{
+			yield return null;
+		}
+
+	}
+		
+
+	//return inline
+	public string getLine(){
+		return this.inline;
+	}
+
+	//pings uBit to send infomation
+	private void pinguBit(){
+		try{
+		//printDebug("Pinging uBit");
+		this.stream.WriteLine ("SEND");
+		this.stream.BaseStream.Flush ();
+		}
+		catch(System.Exception e){
+			if (e is System.IO.IOException) {
+				//Catches an unplogged exception, ignores timeouts
+				printDebug ("catch");
+				StartCoroutine (retryConnection ());
+			}
+		}
+	}
+
+
+	//attempts 	to get the line from the buffer until the timeout value set
+	private string readStream(){
+		try{
+			String line = stream.ReadLine();
+			//printDebug(line);
+			return line;
+		}catch(System.Exception e){
+			//printDebug ("Error with reading stream");
+			//printDebug (e);
+			return null;
+		}
+	}
+
+	//Opens the stream
+	private void openStream(){
+		printDebug ("Initialised");
+		try{
+		this.stream.Open();
+		}
+		catch(System.IO.IOException e) {
+			//printDebug ("Something went wrong with opening stream");
+			//printDebug (e);
+
+			//Attempt to reconnect
+			StartCoroutine (retryConnection ());
+		}
+	}
+	//Closing connection
+	void OnDisable(){
+		try{
+		this.stream.Close ();
+		printDebug("Closing Stream");
+		}
+		catch(System.IO.IOException){
+		}
+		catch(System.Exception){
+		}
+	}
+
+
+	//On a disconnect, attempt to reopen the connection
+	public IEnumerator retryConnection(){
+		printDebug("retrying");
+		do {
+			try {
+				this.stream.Close ();
+				this.stream.Open();
+			} catch (System.Exception) {
+				
+			}
+			yield return new WaitForSeconds (0.016f);
+		} while(!stream.IsOpen);
+	
+		printDebug("Ending Retrys");
+		yield break;
+
+	}
+
+
+	private void printDebug(String s){
+		if (debug == true) {
+			Debug.Log (s);
+		}
+	}
+
+
+
+}
