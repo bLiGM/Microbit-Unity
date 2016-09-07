ad14d4b6af6c6b315d29d704100e1dd0f1448b4d
bLiGM <ucabbl2@ucl.ac.uk>
2016-09-07 17:39:20 +0100

    Changed scripts to use static methods and variables

---
+++ Assets/Scripts/Controllers/uBitInput.cs
@@ -0,0 +1,291 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class uBitInput : MonoBehaviour {
+
+
+	public static bool isSerialInit = false;
+
+	//rotation + button presses from the serial input
+	static private int[] baseRotations = new int[3];
+	static private int[] rotation = new int[3];
+	static private bool buttonAPressed = false;
+	static private bool buttonBPressed = false;
+	static private bool buttonAHeld = false;
+	static private bool buttonBHeld = false;
+	static private float buttonAStart;
+	static private float buttonBStart;
+	static private bool buttonAHoldCheck;
+	static private bool buttonBHoldCheck;
+	static private bool doReset = true;
+	static private float delayInputTimer;
+
+
+	private bool debug = false;
+
+
+
+	// Use this for initialization
+	void Awake () {
+
+		//if an instance of this object already exists, destroy this.
+		if (isSerialInit == true) {
+			printDebug ("Destorying SerialToController");
+			Destroy (this.gameObject);
+		}
+		isSerialInit = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		updateInputs (SerialStream.getLine());
+		printDebug (getRotations().ToString());
+	}
+
+	/// <summary>
+	/// Updates the inputs.
+	/// </summary>
+	/// <param name="s">S.</param>
+	private void updateInputs(string s){
+		//updates the logic depending on the input of the form
+		//button,x,y,z
+		if (Time.realtimeSinceStartup < delayInputTimer) {
+			rotation [0] = 0;
+			rotation [1] = 0;
+			rotation [2] = 0;
+			buttonAPressed = false;
+			buttonBPressed = false;
+			buttonAHeld = false;
+			buttonBHeld = false;
+		} else {
+			try {
+				if (s.Equals ("Reset")) {
+					//do resets
+					doReset = true;
+					return;
+				}
+
+				char[] seperator = { ',' };
+				string[] splitString = s.Split (seperator, 4);
+				// if any of them are null, exception will be thrown and the line will be ignored.
+				if (splitString [0].Equals ("A")) {
+					buttonAPressed = true;
+					buttonBPressed = false;
+					buttonBHeld = false;
+					buttonBHoldCheck = false;
+					checkButtonA ();
+				} else if (splitString [0].Equals ("B")) {
+					buttonBPressed = true;
+					buttonAPressed = false;
+					buttonAHeld = false;
+					buttonAHoldCheck = false;
+					checkButtonB ();
+				} else if (splitString [0].Equals ("C")) {
+					buttonAPressed = true;
+					buttonBPressed = true;
+
+					checkButtonA ();
+					checkButtonB ();
+
+				} else {
+					buttonAPressed = false;
+					buttonBPressed = false;
+					buttonAHoldCheck = false;
+					buttonBHoldCheck = false;
+					buttonAHeld = false;
+					buttonBHeld = false;
+
+
+				}
+
+				string roll = splitString [1];
+				string pitch = splitString [2];
+				string yaw = splitString [3];
+
+				rotation [0] = int.Parse (roll);
+				rotation [1] = int.Parse (pitch);
+				rotation [2] = int.Parse (yaw);
+
+
+				if (doReset) {
+					baseRotations [0] = int.Parse (roll);
+					baseRotations [1] = int.Parse (pitch);
+					baseRotations [2] = int.Parse (yaw);
+					doReset = false;
+				}
+
+
+			} catch (System.Exception) {
+				return;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Checks the button a.
+	/// </summary>
+	private void checkButtonA(){
+		if(buttonAHoldCheck == false){
+			buttonAHoldCheck = true;
+			buttonAStart = Time.realtimeSinceStartup;
+		}
+		else if(buttonAHoldCheck == true){
+			if((Time.realtimeSinceStartup - buttonAStart)<0.25){
+				//Do nothing
+				buttonAHeld = false;
+			}
+			else{
+				buttonAHeld = true;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Checks the button b.
+	/// </summary>
+	private void checkButtonB(){
+		if(buttonBHoldCheck == false){
+			buttonBHoldCheck = true;
+			buttonBStart = Time.realtimeSinceStartup;
+		}
+		else if(buttonBHoldCheck == true){
+			if((Time.realtimeSinceStartup - buttonBStart)<0.25){
+				//Do nothing
+				buttonBHeld = false;
+			}
+			else{
+				buttonBHeld = true;
+			}
+		}
+	}
+
+
+	/// <summary>
+	/// Gets A pressed.
+	/// </summary>
+	/// <returns><c>true</c>, if A pressed was gotten, <c>false</c> otherwise.</returns>
+	static public bool getAPressed(){
+		return buttonAPressed;
+	}
+
+
+	/// <summary>
+	/// Gets the B pressed.
+	/// </summary>
+	/// <returns><c>true</c>, if B pressed was gotten, <c>false</c> otherwise.</returns>
+	static public bool getBPressed(){
+		return buttonBPressed;
+	}
+
+	/// <summary>
+	/// Gets A held.
+	/// </summary>
+	/// <returns><c>true</c>, if A held was gotten, <c>false</c> otherwise.</returns>
+	static public bool getAHeld(){
+		return buttonAHeld;
+	}
+
+	/// <summary>
+	/// Gets the B held.
+	/// </summary>
+	/// <returns><c>true</c>, if B held was gotten, <c>false</c> otherwise.</returns>
+	static public bool getBHeld(){
+		return buttonBHeld;
+	}
+
+	//-------------------------------------------//
+	//Returns the unadjusted roll/pitch/yaw values//
+	//-------------------------------------------//
+	private int getRoll(){
+		return rotation [0];
+	}
+
+	private int getPitch(){
+		return rotation [1];
+	}
+
+	private int getYaw(){
+		return rotation [2];
+	}
+	//-------------------------------------------//
+	//-------------------------------------------//
+	//-------------------------------------------//
+
+	//-------------------------------------------//
+	//Returns the adjusted roll/pitch/yaw values, adjusted with respect to the base rotation//
+	//Adds a deadzone at the extreme rotations to prevent accidental overrotating//
+	//-------------------------------------------//
+	static private int getAdjustedRoll(){
+		//Roll goes from -180 degrees to +180
+		return adjustAngle(rotation[0] - baseRotations[0], 180);
+	}
+
+	static private int getAdjustedPitch(){
+		//Pitch goes from -90 degrees to +90
+		return adjustAngle(rotation[1] - baseRotations[1], 90);
+	}
+
+	static private int getAdjustedYaw(){
+		//Yaw goes from 0 degrees to 360
+		//Since the yaw is VERY sensitive, I decided to put a deadzone in the neutral position aswell//
+		//I also needed to dampen the yaw, since the yaw is extremely noisy
+		if (Mathf.Abs (rotation [2] - baseRotations [2]) < 8) {
+			return 0;
+		} else if (rotation [2] - baseRotations [2] < -8) {
+			return adjustAngle(rotation[2] - baseRotations[2] + 8, 90);
+		}
+		else{
+			return adjustAngle(rotation[2] - baseRotations[2] - 8, 90);
+		}
+	}
+
+	static private int adjustAngle(int a, int limit){
+		int returnVal;
+		if (a > limit) {
+			returnVal = -limit + (limit - a);
+		} else if (a < -limit) {
+			returnVal = limit - (-limit - a);
+		} else {
+			returnVal = a;
+		}
+
+		//----------Deadzone------------//
+		if (returnVal > limit - limit / 3) {
+			return limit - limit / 3;
+		} else if (returnVal < -limit + limit / 3) {
+			return -limit + limit / 3;
+		} else {
+			return returnVal;
+		}
+	}
+
+	//-------------------------------------------//
+	//-------------------------------------------//
+	//-------------------------------------------//
+
+
+	//Returns the rotations as a Unity Vector3//
+	//Adjusts the rotations to match the Unity axes//
+	static public Vector3 getRotations(){
+		//------The rotations must be slightly tweaked to match the Unity axes//
+		return new Vector3 (-getAdjustedPitch(), getAdjustedYaw(), -getAdjustedRoll());
+	}
+
+	private void printDebug(string s){
+		if (debug) {
+			Debug.Log (s);
+		}
+	}
+
+	static public bool isSerialTimeOut(){
+		return SerialStream.isTimingOut ();
+	}
+
+
+	//Delays further input by s seconds
+	static public void delayInput(float s){
+		delayInputTimer = Time.realtimeSinceStartup + s;
+	}
+
+}
--- Assets/Scripts/Serial/SerialStream.cs
+++ Assets/Scripts/Serial/SerialStream.cs
@@ -3,28 +3,26 @@ using System.Collections;
 using System.IO.Ports;
 using System;
 
-
 public class SerialStream : MonoBehaviour {
+
+	public static bool isSerialStreamInit = false;
+
 	private SerialPort stream;
-	private string inline;
+	static private string inline;
 	public string COM;
 	public int BAUD;
 	public int timeout = 0;
-	public static bool isSerialStreamInit = false;
+
 
 	private float lastRecievedTime;
-
-	private bool timingOut = false;
-
+	static private bool timingOut = false;
 	private bool debug = false;
 
-	
+
 
 	// Use this for initialization
 	// Unity does not use constructors, but rather Start() and Awake()
-	void Start () {
-
-
+	void Awake () {
 		//if an instance of this object already exists, destroy this.
 		if (isSerialStreamInit == true) {
 			printDebug ("Destorying duplicate SerialStream");
@@ -40,7 +38,7 @@ public class SerialStream : MonoBehaviour {
 		}
 		openStream ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if ((inline = this.readStream ()) != null) {
@@ -54,7 +52,7 @@ public class SerialStream : MonoBehaviour {
 	//Send and Read CoRoutines
 	public IEnumerator AsyncReaduBit(float timeout, System.Action fail = null){
 		//timeout should be very small (microseconds);
-		float startTime = Time.time;
+		float startTime = Time.realtimeSinceStartup;
 		float currentTime = 0;
 		float diff = 0;
 
@@ -69,22 +67,22 @@ public class SerialStream : MonoBehaviour {
 				readLine = null;
 			}
 			if(readLine != null){
-				this.inline = readLine;
+				inline = readLine;
 				this.stream.BaseStream.Flush();
-				lastRecievedTime = Time.time;
+				lastRecievedTime = Time.realtimeSinceStartup;
 				timingOut = false;
 			}
 			else{
-				if((Time.time - lastRecievedTime)> 1f){
+				if((Time.realtimeSinceStartup - lastRecievedTime)> 1f){
 					//essentially a manual timeout
-					printDebug ((lastRecievedTime-Time.time).ToString());
+					printDebug ((lastRecievedTime-Time.realtimeSinceStartup).ToString());
 					timingOut = true;
 					StartCoroutine(retryConnection());
 				}
 				yield return new WaitForSeconds(0.016f);
 			}
 
-			currentTime = Time.time;
+			currentTime = Time.realtimeSinceStartup;
 			//difference in milliseconds
 			diff = (currentTime - startTime)*1000;
 		} while(diff < timeout);
@@ -97,19 +95,19 @@ public class SerialStream : MonoBehaviour {
 		}
 
 	}
-		
 
-	//return inline
-	public string getLine(){
-		return this.inline;
+
+	//return current line read by SerialPort
+	static public string getLine(){
+		return inline;
 	}
 
 	//pings uBit to send infomation
 	private void pinguBit(){
 		try{
-		//printDebug("Pinging uBit");
-		this.stream.WriteLine ("SEND");
-		this.stream.BaseStream.Flush ();
+			//printDebug("Pinging uBit");
+			this.stream.WriteLine ("SEND");
+			this.stream.BaseStream.Flush ();
 		}
 		catch(System.Exception e){
 			if (e is System.IO.IOException) {
@@ -142,7 +140,7 @@ public class SerialStream : MonoBehaviour {
 	private void openStream(){
 		printDebug ("Initialised");
 		try{
-		this.stream.Open();
+			this.stream.Open();
 		}
 		catch(System.IO.IOException e) {
 			printDebug ("Something went wrong with opening stream");
@@ -155,8 +153,8 @@ public class SerialStream : MonoBehaviour {
 	//Closing connection
 	void OnDisable(){
 		try{
-		this.stream.Close ();
-		printDebug("Closing Stream");
+			this.stream.Close ();
+			printDebug("Closing Stream");
 		}
 		catch(System.IO.IOException){
 		}
@@ -174,11 +172,11 @@ public class SerialStream : MonoBehaviour {
 					this.stream.Close ();
 					this.stream.Open ();
 				} catch (System.Exception) {
-				
+
 				}
 				yield return new WaitForSeconds (0.016f);
 			} while(!stream.IsOpen);
-	
+
 			printDebug ("Ending Retrys");
 
 			yield break;
@@ -187,7 +185,7 @@ public class SerialStream : MonoBehaviour {
 	}
 
 	//Returns if a timeout is occuring
-	public bool isTimingOut(){
+	static public bool isTimingOut(){
 		return timingOut;
 	}
 
